78f147b89b1470909203227a0d7b094edac70bc4
unknown <gaash.ofir@gmail.com>
2026-02-11 12:59:07 +0200

    tiny bug fix (unrelated to current tasks)

--- src/stats.py
+++ src/stats.py
@@ -232,11 +232,9 @@ def estimate_lambda_min(X, M=10000, batch_g=64, device=None):
 
     A /= M
     A = (A + A.T) * 0.5                                     # symmetrize for numerical safety
-    lam_min = torch.linalg.eigvalsh(A)[0].item()
     try:
         lam_min = torch.linalg.eigvalsh(A)[0].item()
     except Exception:
-        print("Numerical instability occured in estimate_lambda_min. Defaulting to lam_min=1.")
         lam_min = 1
     return lam_min
 
